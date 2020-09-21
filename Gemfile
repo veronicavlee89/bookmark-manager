4c3390c3f498076e14b00f20cee778144beb90c7
cristina <c.fujiname@gmail.com>
2020-09-21 13:59:00 +0100

    Add SimpleCov, Rubocop

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
-source "http://rubygems.org"
-
-gem 'rspec'
 gem 'sinatra'
-gem 'capybara'
 
+group :test do
+  gem 'capybara'
+  gem 'rspec'
+  gem 'rubocop', '0.79.0'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
--- spec/spec/spec_helper.rb
+++ spec/spec/spec_helper.rb
@@ -6,8 +6,19 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+                                                                   SimpleCov::Formatter::Console,
+                                                                   # Want a nice code coverage website? Uncomment this next line!
+                                                                   SimpleCov::Formatter::HTMLFormatter
+                                                               ])
+SimpleCov.start
+
 
 Capybara.app = Bookmark
+
 
 
 
@@ -39,6 +50,12 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
