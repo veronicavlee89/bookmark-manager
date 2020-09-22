5a584e1e522ceedbda749583c7f9b32f29aec1a5
cristina <c.fujiname@gmail.com>
2020-09-22 17:12:16 +0100

    Update feature and unit tests

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,8 +5,7 @@ describe Bookmark do
   describe '.all' do
     it "returns all available bookmarks" do
       bookmarks = Bookmark.all #class method
-      expect(bookmarks).to include("http://www.askjeeves.com")
-      expect(bookmarks).to include("http://www.twitter.com")
+      expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")
       expect(bookmarks).to include("http://www.makersacademy.com")
     end
