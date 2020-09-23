455cd86f07d1ccc8b1d7c84595c0bcd954909908
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-09-23 14:46:15 +0100

    Commented out feature controller test
    test is now obsolete.

--- spec/features/feature_controller_spec.rb
+++ spec/features/feature_controller_spec.rb
@@ -1,7 +1,7 @@
-feature 'Page shows Bookmark Manager' do
-  scenario 'shows Bookmark Manager' do
-    visit('/')
-    expect(page).to have_content 'Bookmark Manager'
-  end
-end
+# feature 'Page shows Bookmark Manager' do
+#   scenario 'shows Bookmark Manager' do
+#     visit('/')
+#     expect(page).to have_content 'Bookmark Manager'
+#   end
+# end
 
